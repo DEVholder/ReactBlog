6c4e439a109887e7be4cbeda1907e2fbdeee014d
Devholder <dev0holder@gmail.com>
2023-03-30 17:50:45 +0900

    23.03.30

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let Logo = 'ReactBlog';
+  let [title, setTitle] = useState(['남자코트 추천','강남 우동맛집','파이썬독학']);
+  let [date, setDate] = useState(['2월 17일 발행','2월 17일 발행','2월 17일 발행']);
+  let [like, setLike] = useState([0,0,0]);
+
+  function LikePlus(li){
+    let tmp = [...like];
+    like[li] = like[li] + 1;
+    setLike(like);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>{Logo}</h4> 
+      </div>
+      <div className='list'>
+        <h4>{title[0]} <span onClick={LikePlus(0)}>👍</span> {like[0]} </h4>
+        <p>{date[0]}</p>
+      </div>
+      <div className='list'>
+        <h4>{title[1]} <span>👍</span> {like[1]} </h4>
+        <p>{date[1]}</p>
+      </div>
+      <div className='list'>
+        <h4>{title[2]} <span>👍</span> {like[2]} </h4>
+        <p>{date[2]}</p>
+      </div>
     </div>
   );
 }
